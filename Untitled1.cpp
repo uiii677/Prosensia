536f7ed0471cb546ab3a5b59e3e1f14d5f7f576c
uiii677 <muhammadumair124124@gmail.com>
2025-06-26 13:47:57 +0500

    Day2

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,17 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main (){
-	string Name ;
-	int Age;
-	float GPA;
 	
-	cout << "Enter your Name, age and GPA : " << endl;
-	cin >> Name >> Age >> GPA ;
+	int Mark1, Mark2 , Mark3 ;
+	float Total , Average ;
 	
-	cout << "Name : " << Name << endl;
-	cout << " Age : "  << Age << endl;
-	cout << " GPA : "  << GPA << endl;
+	cout << "Enter marks of first Subject out of 100 : " << endl;
+	cin >> Mark1 ;
+	  
+	cout << "Enter marks of Second Subject out of 100 : " << endl;
+	cin >> Mark2;
+	
+	cout << "Enter marks of Third Subject out of 100 : " << endl;
+	cin >> Mark3;
+	
+	Total = Mark1 + Mark2 + Mark3 ;
+	Average = Total / 3 ;
+	
+	cout << "Total : " << Total << endl;
+	cout << "Average : " << Average << endl;
+	
+	if (  Average >= 80 ){
+		
+		cout << "You Got Grade A " << endl;
+	}
+	
+	else if ( Average  >= 70 ){
+		
+		cout << "You Got Grade B " << endl;
+	}
+	
+	else if ( Average  >= 60 ){
+		
+		cout << "You Got Grade C " << endl;
+	}
+	  
+	else {
+		
+		cout << "Your Fail " << endl;
+	}
+	
 	
 	return 0;
+	
 }
