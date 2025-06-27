07e2fdb56d06a171d521142089c9fd08c68040c7
uiii677 <muhammadumair124124@gmail.com>
2025-06-27 11:32:30 +0500

    Day4

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,47 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-	
-	int Mark1, Mark2 , Mark3 ;
-	float Total , Average ;
-	
-	cout << "Enter marks of first Subject out of 100 : " << endl;
-	cin >> Mark1 ;
-	  
-	cout << "Enter marks of Second Subject out of 100 : " << endl;
-	cin >> Mark2;
-	
-	cout << "Enter marks of Third Subject out of 100 : " << endl;
-	cin >> Mark3;
-	
-	Total = Mark1 + Mark2 + Mark3 ;
-	Average = Total / 3 ;
-	
-	cout << "Total : " << Total << endl;
-	cout << "Average : " << Average << endl;
-	
-	if (  Average >= 80 ){
-		
-		cout << "You Got Grade A " << endl;
-	}
-	
-	else if ( Average  >= 70 ){
-		
-		cout << "You Got Grade B " << endl;
-	}
-	
-	else if ( Average  >= 60 ){
-		
-		cout << "You Got Grade C " << endl;
-	}
-	  
-	else {
-		
-		cout << "Your Fail " << endl;
-	}
-	
-	
-	return 0;
-	
+int main() {
+    int number, reversed = 0;
+
+    cout << "Enter an integer to reverse: ";
+    cin >> number;
+
+    int original = number;
+
+    bool isNegative = false;
+    if (number < 0) {
+        isNegative = true;
+        number = -number;  // Make positive for reversal
+    }
+
+    while (number != 0) {
+        int digit = number % 10;
+        reversed = reversed * 10 + digit;
+        number = number / 10;
+    }
+
+    if (isNegative)
+        reversed = -reversed;
+
+    cout << "Reversed: " << reversed << endl;
+
+    return 0;
 }
+
