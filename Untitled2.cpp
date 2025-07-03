1c895ced78b809c34f4fa2f021f7bf0a32b83423
uiii677 <muhammadumair124124@gmail.com>
2025-07-03 12:14:17 +0500

    Day8

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,29 +1,66 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int num;
-    cout << "Enter a number to check if it's prime: ";
-    cin >> num;
+bool isOdd(int n) {
+    return n % 2 != 0;
+}
 
-    if (num <= 1) {
-        cout << num << " is not a prime number." << endl;
-        return 0;
+void conditionalSwap(int* x, int* y) {
+    int diff = (*x > *y) ? (*x - *y) : (*y - *x);
+    if (diff > 10) {
+        int* temp = new int(*x);
+        *x = *y;
+        *y = *temp;
+        delete temp;
     }
+}
 
-    bool isPrime = true;
+void printArrayViaPointer(int* arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << *(arr + i) << " @ " << (arr + i) << endl;
+    }
+}
 
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            isPrime = false;
-            break;
+int* findFirstEven(int* arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        if (*(arr + i) % 2 == 0)
+            return (arr + i);
+    }
+    return nullptr;
+}
+
+void incrementOddsByAddress(int* arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        if (isOdd(*(arr + i))) {
+            *(arr + i) += 1;
         }
     }
+}
 
-    if (isPrime)
-        cout << num << " is a prime number." << endl;
+int main() {
+    int a, b;
+    int arr[5];
+
+    cin >> a >> b;
+    for (int i = 0; i < 5; ++i)
+        cin >> arr[i];
+
+    cout << "Before Swap: a = " << a << ", b = " << b << endl;
+    conditionalSwap(&a, &b);
+    cout << "After Swap : a = " << a << ", b = " << b << endl;
+
+    printArrayViaPointer(arr, 5);
+
+    int* evenPtr = findFirstEven(arr, 5);
+    if (evenPtr != nullptr)
+        cout << "First even found: " << *evenPtr << endl;
     else
-        cout << num << " is not a prime number." << endl;
+        cout << "No even number found in array." << endl;
+
+    incrementOddsByAddress(arr, 5);
+    for (int i = 0; i < 5; ++i)
+        cout << arr[i] << " ";
+    cout << endl;
 
     return 0;
 }
